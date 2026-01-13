0b27b19d0c0e75e0f2ab40a97301a44674775bff
NathanErk <enderhunter178@gmail.com>
2026-01-13 23:22:38 +0800

    Add files via upload

---
+++ Repository Console Back-end Practice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+        Console.OutputEncoding = Encoding.UTF8;
+        {
+            await Task.Delay(1000);
+
+            Console.WriteLine("User Detected.");
+            await Task.Delay(1000);
+
+            Console.WriteLine("User Detected.");
+            await Task.Delay(1000);
+
+            Console.WriteLine("User Detected.");
+            await Task.Delay(1000);
+
+            await Typewriter("You are entering restricted area.", 105);
+            Console.WriteLine();
+
+            await Task.Delay(2000);
+
+            await Typewriter("You are not allowed in this Property.", 110);
+            Console.WriteLine();
+
+            await Typewriter("Leave immediately.", 110);
+            Console.WriteLine();
+
+            await Task.Delay(1000);
+
+            Console.WriteLine("Leave.");
+                await Task.Delay(1000);
+
+            Console.WriteLine("Leave.");
+            await Task.Delay(1000);
+
+            Console.WriteLine("Leave.");
+            await Task.Delay(1000);
+
+            static async Task Typewriter(string text, int delay)
+            {
+                foreach (char c in text)
+                {
+                    Console.Write(c);
+                    await Task.Delay(delay);
+                }
+            }
+        }
+    }
+}
